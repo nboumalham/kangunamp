0cac9d715c7802e9bf21e7de1522565e672981f3
nboumalham <nassib@boumalham.com>
2023-03-27 01:51:46 +0200

    fixed icon size and responsivness of header and footer

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,13 +9,28 @@ import { AudioService } from '../services/audio.service';
 })
 export class HeaderComponent implements OnInit {
    
-  statusLogo : string = "s";
+  statusLogo : string = "";
 
   constructor(private audioService: AudioService) {}
 
   ngOnInit(): void {
     this.audioService.getPlayerStatus().subscribe(status => {
-    	this.statusLogo = status;
+    switch (status) {
+      case 'playing':
+        this.statusLogo = "⏵︎";
+        return;
+      case 'paused':
+        this.statusLogo = "⏸︎";
+        return;
+      case 'stopped':
+        this.statusLogo = "";
+        return;
+      case 'loading':
+        this.statusLogo = "⏳︎";
+        return;
+      default:
+        this.statusLogo = status;
+    }
     });
   }
 
